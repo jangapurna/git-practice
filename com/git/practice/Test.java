bef1a0ad61e7618ab91f51cf0dbcfe5836e529c7
Jpurna <jpurna86@gmail.com>
2025-04-27 15:57:48 -0400

    commiting for my practice stage1

---
+++ com/git/practice/Test.java
@@ -0,0 +1,8 @@
+package com.git.practice;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(" stage 1 File initial commit for practice");
+    }
+}
---
+++ com/git/practice/Test2.java
@@ -0,0 +1,8 @@
+package com.git.practice;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.println("Test 2");
+    }
+}
