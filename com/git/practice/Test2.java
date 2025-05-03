2e614b2ce04a00fedf0f35c4ef88f34c32a35b92
Jpurna <jpurna86@gmail.com>
2025-05-03 11:56:27 -0400

    Commit the changes for Purna.

--- com/git/practice/Test2.java
+++ com/git/practice/Test2.java
@@ -3,6 +3,9 @@ package com.git.practice;
 public class Test2 {
 
     public static void main(String[] args) {
-        System.out.println("Test 2");
+
+        System.out.println("Test 2 - purna");
+
+
     }
 }
